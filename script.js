44ef6dc1460771b29cbe220feef0848e6a147abf
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-17 18:07:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const makeChange = (c) => {
   // your name here
+	  const literal = [
+    ['q', 0.25],
+    ['d', 0.10],
+    ['n', 0.05],
+    ['p', 0.01]
+  ];
+
+  const ans = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
+
+  for (const [name, value] of literal) {
+    while (cents >= value) {
+      ans[name]++;
+      cents = parseFloat((cents - value).toFixed(2)); // Fix floating-point precision
+    }
+  }
+
+  return ans;
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
